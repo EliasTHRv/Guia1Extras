ad3bf724caeba3ac650782ec54dfd46b6797b6eb
Elias Barreto <eliasgobarreto@gmail.com>
2023-03-28 23:56:57 -0300

    Modif Eje6

--- src/Ejercicio6.java
+++ src/Ejercicio6.java
@@ -7,22 +7,37 @@ Leer la altura de N personas y determinar el promedio de estaturas que se encuen
 debajo de 1.60 mts. y el promedio de estaturas en general.
  */
 public class Ejercicio6 {
-    public static void main(String[] args){
-        int perS1;
-        double altuRa;
+
+    public static void main(String[] args) {
+        int perS1, contador, nPer, contador2;
+        double altuRa, promA, promB, sumaA, sumaB;
+        contador = 1;
+        contador2 = 1;
+        promA = 0;
+        promB = 0;
+        sumaA = 0;
+        sumaB = 0;
         Scanner leer = new Scanner(System.in);
         Scanner leer1 = new Scanner(System.in);
         System.out.println("Ingrese la cantidad de personas: ");
-        perS1= leer.nextInt();
-        
-        for (int i = 0; i < perS1; i++) {
+        perS1 = leer.nextInt();
+
+        do {
+            contador = contador + 1;
+
             System.out.println("Ingrese la altura de las personas: ");
-           altuRa= leer1.nextDouble();
-            
-                
+            altuRa = leer1.nextDouble();
+            sumaB = sumaB + altuRa;
+            promB = (sumaB/perS1);
+        } while (contador <= perS1);
+        if (altuRa < 1.6) {
+                contador2 = contador2 +1;
+                sumaA = sumaA + altuRa;
+                promA = (sumaA/contador2);
             }
-        }
-        
+        System.out.println("El promedio de estaturas debajo de 1.6 es: " + promA);
+        System.out.println("El promedio general de alturas es: " + promB);
+
     }
-    
+
 }
